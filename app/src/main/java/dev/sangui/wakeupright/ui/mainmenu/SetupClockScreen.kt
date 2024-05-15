48478a2efe73e07475f1f9f15086dd988019d7f9
Brian Giannini <brian.giannini.1@gmail.com>
2024-05-15 22:41:01 +0200

    force dark theme and make ui fully responsive

--- app/src/main/java/dev/sangui/wakeupright/ui/mainmenu/SetupClockScreen.kt
+++ app/src/main/java/dev/sangui/wakeupright/ui/mainmenu/SetupClockScreen.kt
@@ -1,11 +1,15 @@
 package dev.sangui.wakeupright.ui.mainmenu
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -24,89 +28,116 @@ import androidx.compose.ui.unit.sp
 import dev.sangui.wakeupright.alarm.DataStoreManager
 import kotlinx.coroutines.flow.collectLatest
 import java.time.format.DateTimeFormatter
+import java.util.Locale
 
 
 @Composable
 fun SetupClockScreen(setupClockViewModel: SetupClockViewModel, dataStoreManager: DataStoreManager) {
-    Column(
+    Box(
         modifier = Modifier
             .fillMaxSize()
             .padding(all = 20.dp),
-        horizontalAlignment = Alignment.CenterHorizontally,
+        contentAlignment = Alignment.Center
     ) {
-        var selectedHour by remember { mutableIntStateOf(0) }
-        var selectedMinute by remember { mutableIntStateOf(0) }
-        var scheduledTime by remember { mutableStateOf<String?>(null) }
+        val scrollState = rememberScrollState()
 
-        LaunchedEffect(setupClockViewModel) {
-            setupClockViewModel.scheduledDate.collectLatest { date ->
-                scheduledTime = date?.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"))
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .verticalScroll(scrollState),
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            var selectedHour by remember { mutableIntStateOf(0) }
+            var selectedMinute by remember { mutableIntStateOf(0) }
+            var scheduledTime by remember { mutableStateOf<String?>(null) }
+
+            LaunchedEffect(setupClockViewModel) {
+                setupClockViewModel.scheduledDate.collectLatest { date ->
+                    scheduledTime = date?.format(DateTimeFormatter.ofPattern("EEEE HH:mm", Locale.getDefault()))
+                }
+            }
+
+            // Top Elements
+            Column(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text("Hour", style = TextStyle(fontSize = 36.sp))
+                NumberPicker(
+                    dataStoreManager = dataStoreManager,
+                    id = "hours",
+                    maxNumbers = 24,
+                    onValueChange = { selectedHour = it }
+                )
+                Spacer(modifier = Modifier.height(10.dp))
+                Text("Minutes", style = TextStyle(fontSize = 36.sp))
+                NumberPicker(
+                    dataStoreManager = dataStoreManager,
+                    id = "minutes",
+                    maxNumbers = 60,
+                    incrementNumber = 5,
+                    onValueChange = { selectedMinute = it }
+                )
+                Spacer(modifier = Modifier.height(25.dp))
+            }
+
+            // Buttons and Scheduled Time
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .weight(1f), // Ajouter ceci pour que la colonne prenne l'espace restant
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center // To distribute buttons evenly
+            ) {
+                Button(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .weight(1f) // Ajouter ceci pour que le bouton prenne l'espace restant verticalement
+                        .padding(vertical = 8.dp),
+                    onClick = {
+                        setupClockViewModel.scheduleAlarm(selectedHour, selectedMinute)
+                    },
+                ) {
+                    Text(
+                        fontSize = 24.sp,
+                        text = "Sleep",
+                        textAlign = TextAlign.Center,
+                    )
+                }
+
+                Button(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .weight(1f) // Ajouter ceci pour que le bouton prenne l'espace restant verticalement
+                        .padding(vertical = 8.dp),
+                    onClick = {
+                        setupClockViewModel.cancelAlarm()
+                    },
+                ) {
+                    Text(
+                        fontSize = 24.sp,
+                        text = "Cancel",
+                    )
+                }
+
+                if (scheduledTime != null) {
+                    Text(
+                        text = "Alarm: $scheduledTime",
+                        style = TextStyle(fontSize = 18.sp),
+                        modifier = Modifier
+                            .align(Alignment.CenterHorizontally)
+                            .padding(vertical = 8.dp),
+                    )
+                } else {
+                    Text(
+                        text = "No alarm scheduled",
+                        style = TextStyle(fontSize = 18.sp),
+                        modifier = Modifier
+                            .align(Alignment.CenterHorizontally)
+                            .padding(vertical = 8.dp),
+                    )
+                }
             }
         }
-
-        Text("Hour", style = TextStyle(fontSize = 36.sp))
-        Spacer(modifier = Modifier.height(25.dp))
-        NumberPicker(
-            dataStoreManager = dataStoreManager,
-            id = "hours",
-            maxNumbers = 24,
-            onValueChange = { selectedHour = it }
-        )
-        Spacer(modifier = Modifier.height(25.dp))
-        Text("Minutes", style = TextStyle(fontSize = 36.sp))
-        NumberPicker(
-            dataStoreManager = dataStoreManager,
-            id = "minutes",
-            maxNumbers = 60,
-            incrementNumber = 5,
-            onValueChange = { selectedMinute = it }
-        )
-        Spacer(modifier = Modifier.height(25.dp))
-
-        Button(
-            modifier = Modifier
-                .height(150.dp)
-                .width(200.dp),
-            onClick = {
-                setupClockViewModel.scheduleAlarm(selectedHour, selectedMinute)
-            },
-        ) {
-            Text(
-                fontSize = 35.sp,
-                lineHeight = 35.sp,
-                text = "Wake me up",
-                textAlign = TextAlign.Center,
-            )
-        }
-
-        Button(
-            modifier = Modifier
-                .height(150.dp)
-                .width(200.dp),
-            onClick = {
-                setupClockViewModel.cancelAlarm()
-            },
-        ) {
-            Text(
-                fontSize = 35.sp,
-                text = "Cancel",
-            )
-        }
-
-        if (scheduledTime != null) {
-            Text(
-                text = "Alarm scheduled at $scheduledTime",
-                style = TextStyle(fontSize = 18.sp),
-                modifier = Modifier.align(Alignment.CenterHorizontally),
-            )
-        } else {
-            Text(
-                text = "No alarm scheduled",
-                style = TextStyle(fontSize = 18.sp),
-                modifier = Modifier.align(Alignment.CenterHorizontally),
-            )
-
-        }
     }
-
 }
